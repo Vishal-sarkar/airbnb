ac92e9251b78878e6268d90bde6c719c0f012f14
unknown <vishalsarkar852@gmail.com>
2023-02-24 00:42:43 +0530

    pushing the search functionality

--- components/Header.js
+++ components/Header.js
@@ -18,9 +18,9 @@ function Header() {
         </div>
 
         {/* Middle */}
-        <div className='flex items-center border-2 rounded-full py-2'>
+        <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm '>
           <input className='flex-grow pl-5 bg-transparent outline-none' type="text" name="" id="" placeholder='Start your search' />
-          <SearchIcon className="hidden md:inline-flex h-8 bg-red-500 text-white rounded-full p-2 cursor-pointer"/>
+          <SearchIcon className="hidden md:inline-flex h-8 bg-red-500 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
           
         </div>
 
